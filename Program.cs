92f79115e7fc572bb4db654010fc5340bb2cc42d
Oleg <>
2020-12-06 17:34:46 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a;
+            double b;
+            double c;
+            char oper;
+
+            Console.WriteLine("Введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор:");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+
+            
+            if (oper == '+')
+            {
+                c = a + b;
+                Console.WriteLine("Cумма " + a + " и " + b + " равна " + c + ".");
+            }
+
+            else if (oper == '-')
+            {
+                c = a - b;
+                Console.WriteLine("Разность " + a + " и " + b + " равна " + c + ".");
+            }
+
+            else if (oper == '*')
+            {
+                c = a * b;
+                Console.WriteLine("Умножение " + a + " на " + b + " равно " + c + ".");
+            }
+
+            else if (oper == '/')
+            {
+                c = a / b;
+                Console.WriteLine("Деление " + a + " на " + b + " равно " + c + ".");
+            }
+            else if (oper == '%')
+            {
+                c = a % b;
+                Console.WriteLine("Остаток " + a + " от " + b + " будет " + c + ".");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестный оператор.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
